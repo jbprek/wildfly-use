54afe0fd23b13a41b8ea1cc048e25f3afc8e2fdc
jbprek <john@prekezes.com>
2015-05-06 07:38:21 +0300

    WIP

--- ejb-cdi-demo/src/main/java/org/bagab/ejbcdi/cdi/alternatives/package-info.java
+++ ejb-cdi-demo/src/main/java/org/bagab/ejbcdi/cdi/alternatives/package-info.java
@@ -1,5 +1,10 @@
 /**
  * Demonstrates the use of CDI alternatives
  *
+ * Scenario:
+ *
+ * One Singleton @Local interface {@see ApplicationSingletonLocal} and two different implementations {@see ApplicationSingletonAlternative1} and  {@see ApplicationSingletonAlternative2}.
+ * The selection of the one to be used is decided in WEB-INF/beans.xml.
+ *
  */
 package org.bagab.ejbcdi.cdi.alternatives;
